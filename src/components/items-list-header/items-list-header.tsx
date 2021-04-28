fca620d6399c5527dc01dc253dbf7653e45b4418
Aleksandr Zhuravlev <ja@idaproject.com>
2021-04-29 00:10:16 +0300

    feat(select): создал компонент для селекта и поправил анимацию

--- src/components/items-list-header/items-list-header.tsx
+++ src/components/items-list-header/items-list-header.tsx
@@ -2,19 +2,45 @@ import React, {FC} from "react";
 import {Link} from 'react-router-dom';
 import styles from "./items-list-header.module.css"
 import {IClassName} from "../../types/common";
+import Select from "../select/select";
+
+const specs = [
+    {
+        label: 'whatever',
+        value: ''
+    },
+    {
+        label: 'helicopter',
+        value: 'helicopter'
+    },
+    {
+        label: 'rocket',
+        value: 'rocket'
+    },
+    {
+        label: 'plane',
+        value: 'plane'
+    },
+    {
+        label: 'airship',
+        value: 'airship'
+    },
+];
 
 const ItemsListHeader: FC<IClassName> = ({className}) => {
+    const clickHandler = (item) => {
+        console.log(item);
+    }
+
     return (
         <section className={`${styles.ItemsListHeader} ${className}`}>
             <div className={styles.wrap}>
                 <h1 className={styles.title}>Rent</h1>
-                <button className={styles.select}>whatever</button>
+                <Select label="whatever" specs={specs} onClick={clickHandler} />
             </div>
 
             <Link to="/add" className={styles.link}>
-                Add new
-
-                <div className={styles.btn} />
+                Add new <div className={styles.btn} />
             </Link>
         </section>
     )
---
+++ src/components/select/select.tsx
@@ -0,0 +1,72 @@
+import React, {FC, useState} from "react";
+import {CSSTransition} from "react-transition-group"
+import "./select.css"
+import {IClassName} from "../../types/common";
+
+interface ISpec {
+    label: string;
+    value: string;
+}
+
+interface ISelectProps extends IClassName {
+    label: string,
+    specs: ISpec[],
+    onClick: (item: ISpec) => void
+}
+
+
+const Select: FC<ISelectProps> =
+    ({
+         label,
+         specs,
+         onClick,
+    }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const List: FC = () => {
+        const clickHandler = (item: ISpec): void => {
+            setIsOpen(!isOpen);
+            onClick(item);
+        }
+        return (
+            <ul className="select__list">
+                {
+                    specs.map((item, i) => (
+                        <li
+                            className="select__item"
+                            key={i}
+                        >
+                            <button
+                                className="select__item-btn"
+                                onClick={() => clickHandler(item)}
+                            >
+                                {item.label}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }
+
+    return (
+        <div className="select">
+            <button
+                className={`select__btn ${isOpen ? '_opened' : ''}`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                {label}
+            </button>
+            <CSSTransition
+                in={isOpen}
+                classNames="appear"
+                unmountOnExit
+                timeout={300}
+            >
+                <List />
+            </CSSTransition>
+        </div>
+    )
+}
+
+export default Select;
