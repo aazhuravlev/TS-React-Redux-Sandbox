12e86a1b62f210da895d7745179295c42b0a2fb7
Aleksandr Zhuravlev <ja@idaproject.com>
2021-05-03 03:19:49 +0300

    feat(backend): добавил проксирование, чтобы запускать фронт и бэк одновременно

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
+
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import router from './router.js';
 
-const PORT = 3000;
+const PORT = 3001;
 const DB_URL = 'mongodb+srv://user:user@cluster0.bcskn.mongodb.net/Ts-React-Redux-Sandbox?retryWrites=true&w=majority';
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.static('static'));
 app.use(fileUpload());
 app.use('/api', router);
+app.use('/api', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
 
 async function startApp() {
   try {
