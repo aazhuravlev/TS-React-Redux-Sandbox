5095fdc3953694aac8ad4632d48fa47a60802cf9
Aleksandr Zhuravlev <ja@idaproject.com>
2021-05-05 01:44:07 +0300

    feat(vehicle-card): добавил компонент

---
+++ src/components/vehicle-card/vehicle-card.tsx
@@ -0,0 +1,35 @@
+import React, {FC} from "react";
+import styles from "./vehicle-card.module.css";
+import {Link} from "react-router-dom";
+import LazyImage from "../lazy-image/lazy-image";
+
+interface IVehicleCardProps {
+    id: string;
+    preview: string;
+    image: string;
+    name: string;
+    description: string;
+    rent: string | number;
+}
+
+const VehicleCard: FC<IVehicleCardProps> = ({id, preview, image, name, description, rent}) => (
+    <Link to={`/vehicles/${id}`} className={styles.VehicleCard}>
+        <LazyImage
+            className={styles.image}
+            preview={preview}
+            src={image}
+            altPreview="Превью транспортного средства"
+            alt="Изображение транспортного средства"
+        />
+
+        <div>
+            <h2 className={styles.title}>{name}</h2>
+            <p className={styles.description}>{description}</p>
+            <span className={styles.rent}>
+                {typeof rent === 'string' ? rent.split(',').join(' ') : rent} $/h
+            </span>
+        </div>
+    </Link>
+)
+
+export default VehicleCard;
